73e4b8709f7e289d1c9418e3ff377c6fdcfd57e5
James Voong <James.Voong.91@gmail.com>
2021-10-31 17:06:07 +0000

    Add views
    🐧

---
+++ support_ticket/resources/views/layouts/default.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File a support ticket</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"/>
+</head>
+<body>
+    <header>
+        <div class="container mx-auto p-4">
+            <h1 class="text-2xl font-bold text-center">
+                File an issue
+            </h1>
+        </div>
+    </header>
+    <main class="p-4">
+        @yield('content')
+    </main>
+</body>
+</html>
