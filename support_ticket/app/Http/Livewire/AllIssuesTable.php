1ede81199b242ee55859b0db193a16bf9690a661
James Voong <James.Voong.91@gmail.com>
2021-11-01 21:55:43 +0000

    Added livewire
    🐧

---
+++ support_ticket/app/Http/Livewire/AllIssuesTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AllIssuesTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.all-issues-table');
+    }
+}
