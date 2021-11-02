a6935c0e846fc1e73d49a0457e2ff61d8e52cfcc
James Voong <James.Voong.91@gmail.com>
2021-11-02 19:53:48 +0000

    all_issues can be displayed as a table
    🐧

--- support_ticket/resources/views/all_issues.blade.php
+++ support_ticket/resources/views/all_issues.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <section>
+        <header>
+            <div class="container mx-auto p-4">
+                <h1 class="text-2xl font-bold text-center">
+                    All issues
+                </h1>
+            </div>
+        </header>
         <livewire:all-issues-table>
     </section>
 @endsection
--- support_ticket/resources/views/livewire/all-issues-table.blade.php
+++ support_ticket/resources/views/livewire/all-issues-table.blade.php
@@ -1,4 +1,36 @@
 <div>
+    <table class="table-auto w-full mb-6">
+        <thead>
+            <tr>
+                <th class="px-4 py-2">First name</th>
+                <th class="px-4 py-2">Last name</th>
+                <th class="px-4 py-2">Address</th>
+                <th class="px-4 py-2">Year at University</th>
+                <th class="px-4 py-2">Degree</th>
+                <th class="px-4 py-2">Operating System</th>
+                <th class="px-4 py-2">Description of Issue</th>
+                <th class="px-4 py-2">Date created</th>
+                <th class="px-4 py-2">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($tickets as $ticket)
+                <tr>
+                    <td class="border px-4 py-2">{{ $ticket->first_name }}</td>
+                    <td class="border px-4 py-2">{{ $ticket->last_name }}</td>
+                    <td class="border px-4 py-2">{{ $ticket->address }}</td>
+                    <td class="border px-4 py-2">{{ $ticket->year_at_uni }}</td>
+                    <td class="border px-4 py-2">{{ $ticket->degree }}</td>
+                    <td class="border px-4 py-2">{{ $ticket->operating_system }}</td>
+                    <td class="border px-4 py-2">{{ $ticket->issue }}</td>
+                    <td class="border px-4 py-2">{{ $ticket->created_at->format('d/m/Y H:i') }}</td>
+                    <!-- TODO Implement status -->
+                    <td class="border px-4 py-2">{{ $ticket->status }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
     @foreach ($tickets as $ticket)
         {{ $ticket }}
     @endforeach
